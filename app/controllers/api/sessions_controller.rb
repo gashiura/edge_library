83fe9548089a317d7ab5370d31193df362a0d251
gashiura <gasi3319@yahoo.co.jp>
2020-09-12 21:42:39 +0900

    ログイン処理にセッションを利用

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,6 +2,7 @@ class Api::SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:email])
     if @user&.authenticate(params[:password])
+      log_in(@user)
       render 'create', formats: 'json', handlers: 'jbuilder'
     else
       render json: { errors: [{ message: "ログインに失敗しました。\nユーザー名かパスワードが間違っています。" }] }
