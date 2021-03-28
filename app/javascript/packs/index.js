90b32bc3671dc4a9cfe9878865e59bc90ead0356
gashiura <gasi3319@yahoo.co.jp>
2021-03-28 23:45:49 +0900

    Rails5.2.5に更新

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -3,9 +3,6 @@ import Router from '../routes/router';
 import Store from '../store/store';
 import App from '../app.vue';
 
-import PaginationNav from 'bootstrap-vue/es/components/pagination-nav';
-Vue.use(PaginationNav);
-
 const vm = new Vue({
   el: '#app',
   router: Router,
